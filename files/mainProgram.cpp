0ebe16e87099e841c7cecda440fd4126cbfba7b0
Pratham <130177487+Pratham-99-Sh@users.noreply.github.com>
2023-07-28 18:23:42 +0000

    commited using codespace

--- files/mainProgram.cpp
+++ files/mainProgram.cpp
@@ -1,74 +1,37 @@
-#include<iostream>
-#include<list>
-#include<queue>
-#include<vector>
-//#include<climits>
-#include<set>
-
+#include<bits/stdc++.h>
 using namespace std;
 
-class Graph{
-    int V;
-    list<pair<int, int>> *adj;
-
-    public:
-    Graph(int size)
+int cntSubarrays(vector<int> arr,int k){
+    //complete this method
+    
+    vector<int> csum;
+    int stiln = 0, res = 0;
+    for(auto x: arr)
     {
-        V = size;
-        adj = new list<pair<int, int>>[V];
+        if(x==k) res++;
+        stiln+=x;
+        csum.push_back(stiln);
     }
-
-    void addEdge(int a, int b, int wt, bool undir=true)
+    int diff = 1;
+    while(diff < arr.size())
     {
-        adj[a].push_back({wt, b});
-        if(undir)
-            adj[b].push_back({wt, a});
-    }
-
-    int dijkstra(int src, int dest)
-    {
-        vector<int> dist(V, __INT_MAX__);
-        dist[src] = 0;
-        set<pair<int, int>> s;
-        s.insert({0, src});
-
-        while(!s.empty())
+        int start = 0, end = start + diff;
+        while(end < arr.size())
         {
-            auto it = s.begin();
-            int node = it->second;
-            int distTillNow = it->first;
-            s.erase(it);
-            for(auto nbrs : adj[node])
-            {
-                int nbr = nbrs.second;
-                int edgeLen = nbrs.first;
-                if(distTillNow + edgeLen < dist[nbr])
-                {
-                    auto f = s.find({dist[nbr], nbr});
-                    if(f != s.end())
-                        s.erase(f);
-                    dist[nbr] = distTillNow + edgeLen;
-                    s.insert({dist[nbr], nbr});
-                }
-            }
+            if(csum[end] - csum[start] == k) res++;
+            start++;
+            end++;
         }
-
-        for(int i=0; i<V; i++)
-            cout<<i<<" is at a distance of "<<dist[i]<<endl;
-
-        return dist[dest];
+        diff++;
     }
-};
-
+    
+    return res;
+    
+}
+ 
 int main()
 {
-    Graph g(5); // 5 vertices numbered from 0 to 4
-    g.addEdge(0, 1, 1);
-    g.addEdge(1, 2, 1);
-    g.addEdge(0, 2, 4);
-    g.addEdge(0, 3, 7);
-    g.addEdge(3, 2, 2);
-    g.addEdge(3, 4, 3);
-    cout<<endl<<g.dijkstra(0, 4)<<endl;
-    return 0;
+    vector<int> arr = {10, 2, -2, -20, 10};
+    int k = -10;
+    cout<<cntSubarrays(arr,k)<<endl;
 }
