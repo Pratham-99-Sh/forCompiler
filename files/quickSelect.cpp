916f742ff2c3c475797f5e69fbd0b823b57d3a89
Pratham <130177487+Pratham-99-Sh@users.noreply.github.com>
2023-08-26 15:34:32 +0000

    quickSelect updated to sparse search

--- files/quickSelect.cpp
+++ files/quickSelect.cpp
@@ -1,37 +1,35 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
 using namespace std;
 
-int partition(vector<int> &arr, int s, int e){
-    int pivot = arr[e];
-    int i = s-1;
-    for(int j=s; j<e; j++){
-        if(arr[j] < pivot){
-            i++;
-            swap(arr[i], arr[j]);
+int stringBinarySearch(vector<string> &arr, int s, int e, string key){
+if(s>e) return -1;
+int mid = (s+e)/2;
+if(arr[mid] == " "){
+    int left = mid-1;
+    int right = mid+1;
+    while(true){
+        if(left<s && right>e) return -1;
+        else if(right<=e && arr[right]!=" "){
+            mid = right;
+            break;
         }
+        else if(left>=s && arr[left]!=" "){
+            mid = left;
+            break;
+        }
+        left--;
+        right++;
     }
-    swap(arr[i+1], arr[e]);
-    return i+1;
 }
-
-int quickSort(vector<int> &arr, int s, int e, const int key){
-    if(s>e) return -1;
-    int pvt = partition(arr, s, e);
-    if(pvt == key) return arr[pvt];
-    if(pvt > key)
-    return quickSort(arr, s, pvt-1, key);
-    else
-    return quickSort(arr, pvt+1, e, key);
+if(arr[mid] == key) return mid;
+if(arr[mid] > key) return stringBinarySearch(arr, s, mid-1, key);
+return stringBinarySearch(arr, mid+1, e, key);
 }
 
 int main(){
-    vector<int> arr = {10, 5, 2, 0, 7, 6, 4};
-    int result =  quickSort(arr, 0, arr.size()-1, 5);
-    for(int i=0; i<arr.size(); i++){
-        cout<<arr[i]<<" ";
-    }
-    cout << endl;
-    cout << result << endl;
-    return 0;
+vector<string> arr = {"at", " ", " ", " ", "ball", " ", " ", "car", " ", " ", "dad", " ", " "};
+
+cout<<stringBinarySearch(arr, 0, arr.size()-1, "at");
+return 0;
 }
