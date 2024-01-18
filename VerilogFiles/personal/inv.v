c5be130a0ef1ce39bf18ff677dc24bc7013aea26
Pratham <130177487+Pratham-99-Sh@users.noreply.github.com>
2024-01-18 10:26:32 +0000

    fixed issue where output always 1

--- VerilogFiles/personal/inv.v
+++ VerilogFiles/personal/inv.v
@@ -2,6 +2,6 @@ module inv(input inp, output oup);
 wire w1;
 supply1 vdd;
 supply0 gnd;
-nmos n1 (oup, vdd, inp);
-pmos p1 (oup, gnd, inp);
+nmos n1 (oup, gnd, inp);
+pmos p1 (oup, vdd, inp);
 endmodule
